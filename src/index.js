a70be7eb588420b254b4f557d4861bad01d06e32
Anna <kaimol@inbox.ru>
2022-01-11 00:02:51 +0300

    init: start Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let str = String(Math.abs(n));
+    result = '';
+
+    for (let i = 0; i < str.length; i++) {
+        result = `${str[i]}${result}`;
+    }
+
+    return Number(result);
 }
